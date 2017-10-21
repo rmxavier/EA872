5c2cd4fc9a6f895f3e33e7494eacf76ae27b6012
Alisson Poli De Carvalho - 116006 <ra116006@le20-2.grad.fee.unicamp.br>
2017-10-21 14:51:31 -0200

    Adicionei o req_to_server

--- server.c
+++ server.c
@@ -28,6 +28,86 @@ FILE * reg_file;
 
 struct command_list * list = NULL;
 
+void req_to_server(FILE *arq_req){
+	//A porta que utilizaremos é 8585
+	unsigned short porta = 8585;
+	char area[1024];			/* area para envio e recebimentede dados	*/
+	struct sockaddr_in cliente;		/* estrutura de informações sobre os clientes	*/
+	struct sockaddr_in servidor;	/* estrutura de informações sobre o servidor	*/
+	int soquete;			/* soquete para aceitação de pedidos de conexão	*/
+	int novo_soquete;			/* soquete de conexão aos clientes	*/
+	int nome_compr;			/* comprimento do nome de um cliente	*/
+	int mensagem_compr;			/* comprimento da mensagem recebida	*/
+	int i;
+     /* Ligue o soquete ao endereço do servidor.
+	/*
+	* Crie um soquete para aceitar pedidos de conexão.
+	*/
+	if ((soquete = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+	{
+		perror("Erro em socket()");
+		exit(2);
+	}
+
+	/*
+	*/
+	servidor.sin_family 	= AF_INET;
+	servidor.sin_port   	= htons(porta);
+	servidor.sin_addr.s_addr	= INADDR_ANY;
+
+	if (bind(soquete, (struct sockaddr *)&servidor, sizeof(servidor)) < 0)
+	{
+	perror("Erro em bind().\n");
+	exit(3);
+	}
+
+	/*
+	* Aguarde por pedidos de conexão com um fila de até 5 pedidos.
+	*/
+	if (listen(soquete, 5) != 0)
+	{
+		perror("Erro em listen().\n");
+		exit(4);
+	}
+
+	
+	while (1) {
+
+	/*
+	* Aceite um pedido de conexão.
+	*/
+	nome_compr = sizeof(cliente);
+	if ((novo_soquete = accept(soquete, (struct sockaddr *)&cliente, &nome_compr)) == -1)
+	{
+		perror("Erro em accept().\n");
+		break;
+	}
+
+	/*
+	* Receba a mensagem no novo soquete.
+	*/
+	if ((mensagem_compr = recv(novo_soquete, area, sizeof(area), 0)) == -1)
+	{
+		perror("Erro em recv().\n");
+		break;
+	}
+
+	
+	/* 
+	* Imprima o que recebeu e feche a conexão.
+	*/
+	printf("\nMensagem recebida:\n");
+	for(i = 0; i < mensagem_compr; i++) printf("%c", area[i]);
+	fflush(arq_req);
+	close(novo_soquete);
+
+	}  /* Laço termina aqui */
+
+	close(soquete); 
+	printf("O servidor terminou com erro.\n");
+	exit(5);
+}
+
 void print_request_to_file() {
 	command_list * current = list;
 	
@@ -140,6 +220,7 @@ int main(int argc, char** argv)
 		
 	printf("Argumentos:\n  (ENV) webspace: %s\n  (1) path: %s\n  (2) arq_req: %s\n  (3) arq_res: %s\n  (4) arq_reg: %s\n\n", webspace, path, arq_req, arq_res, arq_reg);
 
+	//aqui eu chamo o req_to_server
 	/* argv[1] -> Arquivo contendo a requisicao */
 	if((req_file = fopen(arq_req, "r")) == NULL){
 		printf("Error to open file %s.\n", arq_req);
@@ -176,7 +257,11 @@ int main(int argc, char** argv)
 	/* Funcao para obter resultado do parser */
 	result = get_parsed_request();
 	
+<<<<<<< Updated upstream
 	error = acesso(webspace, result->params->param, result->command, resp_file);
+=======
+	error = acesso("/home/EC11/ra116006/meu-webspace", result->params->param, result->command, resp_file);
+>>>>>>> Stashed changes
 
 	/* Fechando o arquivo... */
 	close(req_file);
